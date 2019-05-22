04e5137e57eb455728e7ca0b51bc304ab24fee61
Marcin <marcin05031997@gmail.com>
2019-05-22 21:57:19 +0200

    add CRUD operations

--- src/main/java/DomParser.java
+++ src/main/java/DomParser.java
@@ -3,10 +3,8 @@ import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class DomParser {
     private SAXReader reader;
@@ -15,15 +13,31 @@ public class DomParser {
     public DomParser(String filePath) throws DocumentException {
         reader= new SAXReader();
         this.document=reader.read(filePath);
-        System.out.println(document);
+    }
+
+    public void addOption(Scanner sc) throws IOException {
+        System.out.println("Customer ID(needs to start with \"A\"-for men or \"B\"-for women and needs to have 6 digits):");
+        sc.nextLine();
+        String id=sc.nextLine();
+        System.out.println("Firstname: ");
+        String firstname=sc.nextLine();
+        System.out.println("Lastname: ");
+        String lastname=sc.nextLine();
+        System.out.println("Phone(9 digits): ");
+        String phone=sc.nextLine();
+        System.out.println("Street: ");
+        String street=sc.nextLine();
+        System.out.println("City: ");
+        String city=sc.nextLine();
+        System.out.println("Country: ");
+        String country=sc.nextLine();
+        addCustomer(id,firstname,lastname,phone,street,city,country);
     }
 
     public void addCustomer(String id,String firstname,String lastname,String phone,String street,String city,String country) throws IOException {
 
         List<Node> nodes = document.selectNodes("//library/users/customers");
-        // nodes will have all the child nodes under your Xpath.
         for (Node node : nodes) {
-            //get the required node and add your new node to specific node.
             if(node instanceof Element) {
                 Element e = (Element) node;
                 Element customer=e.addElement("customer");
@@ -45,52 +59,124 @@ public class DomParser {
             }
         }
         print();
+        //save
 
     }
-
-    public void updateCustomer(String cusID,String id,String firstname,String lastname,String phone,String street,String city,String country) throws IOException {
-//        Node node = document.selectSingleNode("//library/users/customers/customer[@customer_id = '"+id+"']" );
-//
-//        Element customer = (Element) node;
-//        Element
-
-        List<Node> nodes = document.selectNodes("//library/users/customers/customer[@customer_id = '"+cusID+"']" );
-        // nodes will have all the child nodes under your Xpath.
-        for (Node node : nodes) {
-            //get the required node and add your new node to specific node.
-            if(node instanceof Element) {
-                Element e = (Element) node;
-                e.element("firstname").setText(firstname);
-                e.element("lastname").setText(lastname);
-                e.element("phone").setText(phone);
-                e.element("address").element("street").setText(street);
-                e.element("address").element("city").setText(city);
-                e.element("address").element("country").setText(country);
-            }
+    public void deleteOption(Scanner sc) throws IOException {
+        System.out.println("Which customer do you want to delete?");
+        List<Node>customers=getCustomers();
+        System.out.println("0. Return");
+        int a=sc.nextInt();
+        if(a!=0 && a<=customers.size()){
+            System.out.println(customers.get(a-1).valueOf("@customer_id"));
+            deleteCustomer(customers.get(a-1).valueOf("@customer_id"));
         }
-        print();
+
     }
 
     public void deleteCustomer(String id) throws IOException {
         Node node= document.selectSingleNode("//library/users/customers/customer[@customer_id = '"+id+"']");
         node.detach();
         print();
+        //save
     }
 
-    public void getNodes(String nodePath){
+    public void modifyOption(Scanner sc) throws IOException {
+        System.out.println("Which customer do you want to modify?");
+        List<Node> customers= getCustomers();
+        int a=sc.nextInt();
+        if(a!=0&& a<=customers.size()){
+            String customerID=customers.get(a-1).valueOf("@customer_id");
+            Node customer=document.selectSingleNode("//library/users/customers/customer[@customer_id = '"+customerID+"']");
+            Element updatedCustomer= (Element) customer;
+            System.out.println("What do you want to update?");
+            List<String> elements=getContent(updatedCustomer);
+            int b=sc.nextInt();
+            if(b!=0&& b<=elements.size()){
+                if(b<3) {
+                    System.out.println("Old value: " + updatedCustomer.element(elements.get(b-1)).getText());
+                    System.out.println("Enter new value: ");
+                    sc.nextLine();
+                    String updatedValue=sc.nextLine();
+                    updatedCustomer.element(elements.get(b - 1)).setText(updatedValue);
+                }else {
+                    System.out.println("Old value: " + updatedCustomer.element("address").element(elements.get(b-1)).getText());
+                    System.out.println("Enter new value: ");
+                    sc.nextLine();
+                    String updatedValue=sc.nextLine();
+                    updatedCustomer.element("address").element(elements.get(b - 1)).setText(updatedValue);
+                }
 
-        List<Node> nodes = document.selectNodes(nodePath);
-        for(Node n: nodes){
-            n.asXML();
+            }
         }
-        System.out.println("\n");
+        print();
+    }
+
+    public List<Node> getCustomers(){
+        List<Node> nodes = document.selectNodes("/library/users/customers/customer");
+        int a=1;
+        for( Node node: nodes){
+            System.out.println(a+". ID of customer: "+node.valueOf("@customer_id")
+                    + "  Firstname: " + node.selectSingleNode("firstname").getText()
+                    + "  Lastname: " + node.selectSingleNode("lastname").getText()
+                    +"  Phone: "+node.selectSingleNode("phone").getText()
+                    +"  Street: "+node.selectSingleNode("address/street").getText()
+                    +"  City: "+ node.selectSingleNode("address/city").getText()
+                    +"  Country: "+node.selectSingleNode("address/country").getText());
+            a++;
+        }
+        return nodes;
+    }
+
+    public void sort(String path) throws IOException {
+        List<Node> nodes = this.document.selectNodes(path);
+        Collections.sort(nodes, Comparator.comparing(Node::getStringValue));
+        int a=1;
+        for (Node node : nodes) {
+            System.out.println(a+". ID of customer: "+node.valueOf("@customer_id")
+                    + "  Firstname: " + node.selectSingleNode("firstname").getText()
+                    + "  Lastname: " + node.selectSingleNode("lastname").getText()
+                    +"  Phone: "+node.selectSingleNode("phone").getText()
+                    +"  Street: "+node.selectSingleNode("address/street").getText()
+                    +"  City: "+ node.selectSingleNode("address/city").getText()
+                    +"  Country: "+node.selectSingleNode("address/country").getText());
+            a++;
+        }
+        print();
     }
 
     public void print() throws IOException {
         OutputFormat format = OutputFormat.createPrettyPrint();
+        Writer w=new OutputStreamWriter(new FileOutputStream(this.document.getName()));
         XMLWriter writer;
-        writer = new XMLWriter( System.out, format );
+        writer = new XMLWriter( w, format );
         writer.write( document );
+        writer.close();
+    }
+
+    public List<String> getContent(Element element) {
+        StringBuilder builder = new StringBuilder();
+        List<String> nodes=new ArrayList<String>();
+        int a=1;
+        for (Iterator<Element> i = element.elementIterator(); i.hasNext();) {
+            Element e = i.next();
+            if(e.isTextOnly()) {
+                nodes.add(e.getName());
+                builder.append(a + ". " + e.getName() + "\n");
+                a++;
+            }else{
+//                builder.append(a+" ."+ e.elements().get(0).toString());
+                for(Iterator<Element> j = e.elementIterator(); j.hasNext();){
+                    Element child=j.next();
+                    nodes.add(child.getName());
+                    builder.append(a + ". " + child.getName() + "\n");
+                    a++;
+                }
+
+            }
+        }
+        System.out.println(builder.toString());
+        return nodes;
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,48 @@
 import org.dom4j.DocumentException;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws DocumentException, IOException {
-        String filePath="library.xml";
+        String filePath="src/main/resources/library1.xml";
         DomParser dom= new DomParser(filePath);
-        dom.getNodes("/library");
-        dom.addCustomer("A123098","Bolek","Olek","123456789","Lawendowa 12","Gdansk","Polska");
-        dom.updateCustomer("A123098","A123098","Bwerolek","Olewerwk","123456789","Lawenwerdowa 12","Gdansk","Polska");
-        dom.deleteCustomer("A123098");
+        System.out.println("Hello in Library");
+        Scanner sc= new Scanner(System.in);
+        int a=0;
+        while(a!=6){
+            printOptions();
+            a=sc.nextInt();
+            switch (a){
+                case 1:
+
+                    dom.addOption(sc);
+                    //dom.addCustomer("A123098","Bolek","Olek","123456789","Lawendowa 12","Gdansk","Polska");
+                    break;
+                case 2:
+                    dom.deleteOption(sc);
+                    break;
+                case 3:
+                    dom.getCustomers();
+                    break;
+                case 4:
+                    dom.modifyOption(sc);
+                    break;
+                case 5:
+                    dom.sort("/library/users/customers/customer");
+                    break;
+            }
+
+        }
+    }
+
+    public static void printOptions(){
+        System.out.println("What do you want to do ?");
+        System.out.println("1. Add customer to library");
+        System.out.println("2. Delete customer from library");
+        System.out.println("3. Show all customers");
+        System.out.println("4. Modify customer data");
+        System.out.println("5. Sort");
+        System.out.println("6. Exit");
     }
 }
